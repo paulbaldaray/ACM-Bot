cbdf1842b63a4a06d23f8f9f5bce7af34700bdcb
Paul Baldaray <paulbaldaray@gmail.com>
2021-01-02 05:36:49 +0000

    Fixing voice channels on init

--- ACM-bot.py
+++ ACM-bot.py
@@ -2,25 +2,46 @@ import os, discord, dotenv, asyncio
 
 dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-CLIENT = discord.Client()
+
+INTENTS=discord.Intents.default()
+INTENTS.members = True
+CLIENT = discord.Client(intents=INTENTS)
+
+JOIN_VC = 'join-to-create-new-vc'
+NEW_VC = '-'*25
+assert(JOIN_VC != NEW_VC and NEW_VC != '')
 
 @CLIENT.event
 async def on_ready():
+	active_channels = []
+
 	print(f'{CLIENT.user} is connected to the following guild(s):')
 	for guild in CLIENT.guilds:
 		print(f'{guild.name}(id: {guild.id})')
+		for vc in list(guild.voice_channels):
+			if vc.name == JOIN_VC and len(vc.members) > 0:
+				active_channels.append(vc)
+			elif vc.name != JOIN_VC and len(vc.members) == 0:
+				await vc.delete()
+
+	for vc in active_channels:
+		print(f'Creating new channel: {vc.name}')
+		new_channel = await vc.clone(name=NEW_VC)
+		print(f'Moving users:')
+		for member in vc.members:
+			print(member.name)
+			await member.move_to(new_channel)
 
 @CLIENT.event
 async def on_voice_state_update(member, before, after):
-	join_vc = 'join-to-create-new-vc'
 
 	if before.channel\
-			and before.channel.name != join_vc\
-			and len(before.channel.members) == 0:
+			and before.channel.name != JOIN_VC\
+			and len(before.channel.voice_states) == 0:
 		await before.channel.delete()
 
-	if after.channel and after.channel.name == join_vc:
-		new_channel = await after.channel.clone(name='-'*25)
+	if after.channel and after.channel.name == JOIN_VC:
+		new_channel = await after.channel.clone(name=NEW_VC)
 		await member.move_to(new_channel)
 
 CLIENT.run(TOKEN)
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup python3 -u ACM-bot.py > bot.log 2>&1 &
